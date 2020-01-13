cab8937f2c8ce02ed6073e04c2179ad7cd788b81
jeanstkng <jjg.castillo@yavirac.edu.ec>
2020-01-13 10:05:12 -0500

    Fixed state errors on react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Register from './components/Register'
+import Register from './components/pages/Register'
+import AuthState from "./context/authContext/AuthState"
+import setToken from './utils/setToken'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+if (localStorage.token) {
+  setToken(localStorage.token)
+}
 
 function App() {
   return (
-    <Register/>
+    <AuthState>
+      <Router>
+        <Register/>
+      </Router>
+    </AuthState>
   );
 }
 
